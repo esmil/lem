46caa71604a2fded85fd979d0593ec4789ec87d3
Emil Renner Berthing <esmil@mailme.dk>
2012-01-25 21:16:20 +0100

    update copyright information

--- repl.lua
+++ repl.lua
@@ -1,6 +1,6 @@
 --
 -- This file is part of LEM, a Lua Event Machine.
--- Copyright 2011 Emil Renner Berthing
+-- Copyright 2011-2012 Emil Renner Berthing
 --
 -- LEM is free software: you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License as
