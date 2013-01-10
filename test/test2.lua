3650b953cf452493694e68bad592fd3ada371d74
Emil Renner Berthing <esmil@mailme.dk>
2013-01-10 23:49:52 +0100

    Re-license as LGPL
    Acked-by: Asbjørn Sloth Tønnesen <asbjorn@asbjorn.biz>

--- test/test2.lua
+++ test/test2.lua
@@ -3,18 +3,18 @@
 -- This file is part of LEM, a Lua Event Machine.
 -- Copyright 2011-2012 Emil Renner Berthing
 --
--- LEM is free software: you can redistribute it and/or
--- modify it under the terms of the GNU General Public License as
+-- LEM is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as
 -- published by the Free Software Foundation, either version 3 of
 -- the License, or (at your option) any later version.
 --
--- LEM is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- LEM is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+-- GNU Lesser General Public License for more details.
 --
--- You should have received a copy of the GNU General Public License
--- along with LEM.  If not, see <http://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with LEM.  If not, see <http://www.gnu.org/licenses/>.
 --
 
 print('Entered ' .. arg[0])
