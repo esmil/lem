3fc72f58396449be23960ad686ab58d6dcbc8d51
Halfdan Mouritzen <halfdan@robothangarskib.dk>
2013-01-21 15:09:39 +0100

    Adding HTTP status codes

--- lem/http/server.lua
+++ lem/http/server.lua
@@ -32,10 +32,12 @@ require 'lem.http'
 local M = {}
 
 local status_string = {
+	-- 1xx Informational
 	[100] = '100 Continue',
 	[101] = '101 Switching Protocols',
 	[102] = '102 Processing',                     -- WebDAV
 
+	-- 2xx Success
 	[200] = '200 OK',
 	[201] = '201 Created',
 	[202] = '202 Accepted',
@@ -44,7 +46,11 @@ local status_string = {
 	[205] = '205 Reset Content',
 	[206] = '206 Partial Content',
 	[207] = '207 Multi-Status',                   -- WebDAV
+	[208] = '208 Already Reported',               -- WebDAV
+	[226] = '226 IM Used',
+	[230] = '230 Authentication Successfull',
 
+	-- 3xx Redirection
 	[300] = '300 Multiple Choices',
 	[301] = '301 Moved Permanently',
 	[302] = '302 Found',
@@ -53,22 +59,53 @@ local status_string = {
 	[305] = '305 Use Proxy',
 	[306] = '306 Switch Proxy',
 	[307] = '307 Temporary Redirect',
+	[308] = '308 Permanent Redirect',
 
+	-- 4xx Client Error
 	[400] = '400 Bad Request',
 	[401] = '401 Unauthorized',
 	[402] = '402 Payment Required',
 	[403] = '403 Forbidden',
 	[404] = '404 Not Found',
 	[405] = '405 Method Not Allowed',
-	-- ...
+	[406] = '406 Not Acceptable',
+	[407] = '407 Proxy Authentication Required',
+	[408] = '408 Request Timeout',
+	[409] = '409 Conflict',
+	[410] = '410 Gone',
+	[411] = '411 Length Required',
+	[412] = '412 Precondition Failed',
+	[413] = '413 Request Entity Too Large',
+	[414] = '414 Request-URI Too Long',
+	[415] = '415 Unsupported Media Type',
+	[416] = '416 Requested Range Not Satisfiable',
 	[417] = '417 Expectation Failed',
+	-- ...
+	[422] = '422 Unprocessable Entity',           -- WebDAV
+	[423] = '423 Locked',                         -- WebDAV
+	[424] = '424 Failed Dependency',              -- WebDAV
+	-- ...
+	[426] = '426 Upgrade Required',
+	[428] = '428 Precondition Required',
+	[429] = '429 Too Many Requests',
+	[431] = '431 Request Header Fields Too Large',
 
+	-- 5xx Server Error
 	[500] = '500 Internal Server Error',
 	[501] = '501 Not Implemented',
-	-- ...
+	[502] = '502 Bad Gateway',
+	[503] = '503 Service Unavailable',
+	[504] = '504 Gateway Timeout',
 	[505] = '505 HTTP Version Not Supported',
+	[506] = '506 Variant Also Negotiates',
+	[507] = '507 Insufficient Storage',           -- WebDAV
+	[508] = '508 Loop Detected',                  -- WebDAV
 	-- ...
+	[510] = '510 Not Extended',
+	[511] = '511 Network Authentication Required',
+	[531] = '531 Access Denied',
 }
+
 M.status_string = status_string
 
 function M.not_found(req, res)
